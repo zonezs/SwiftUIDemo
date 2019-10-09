9deaef8bb52618cae4d23ab6bbc6e611635335c5
zonezs <zonezs123@gmail.com>
2019-10-09 09:46:44 +0800

    init SwiftUIDemo

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by _zone on 2019/10/9.
+//  Copyright © 2019 _zone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("next for demo ok about can make some okkkkkkkkkkkkkkkkkkjfkdflsfkljfklsfjlfjljf ")
+    }
+}
+
+struct cont: View {
+    var body: some View {
+        Text("fldskfldsfdfjlkdsfldfjkldj")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ContentView()
+        
+    }
+}
