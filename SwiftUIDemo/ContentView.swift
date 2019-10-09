49cf1957116a5e6a388bcff6db1723e1c48c5a49
zonezs <zonezs123@gmail.com>
2019-10-09 14:35:27 +0800

    complete for Apple SwiftUIDemo

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -11,26 +11,34 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.green)
+        
+        VStack {
             
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.green)
+                
+                HStack {
+                    Text("大柳树森林公园 Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
-    }
-}
-
-struct cont: View {
-    var body: some View {
-        Text("fldskfldsfdfjlkdsfldfjkldj")
     }
 }
 
@@ -38,6 +46,5 @@ struct ContentView_Previews: PreviewProvider {
     
     static var previews: some View {
         ContentView()
-        
     }
 }
---
+++ SwiftUIDemo/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MKMapView.swift
+//  SwiftUIDemo
+//
+//  Created by _zone on 2019/10/9.
+//  Copyright © 2019 _zone. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView.init(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MKMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
